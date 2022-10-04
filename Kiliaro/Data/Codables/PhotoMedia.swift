4731e1fce8b889a636f4d429fd376bd79ef85532
Little Developers <littledevelopers@Littles-MacBook-Pro.local>
2022-10-04 17:05:09 +0300

    Add Photo Codables

---
+++ Kiliaro/Data/Codables/PhotoMedia.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoMedia.swift
+//  Kiliaro
+//
+//  Created by Little Developers on 04/10/2022.
+//
+
+import Foundation
+
+// MARK: - PhotoMedia
+struct PhotoMedia: Codable {
+    let id: String?
+    let userID: String?
+    let mediaType: String?
+    let filename: String?
+    let size: Int?
+    let createdAt: Date?
+    let takenAt, guessedTakenAt: String?
+    let md5Sum: String?
+    let contentType: String?
+    let video: String?
+    let thumbnailURL, downloadURL: String?
+    let resx, resy: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID = "user_id"
+        case mediaType = "media_type"
+        case filename, size
+        case createdAt = "created_at"
+        case takenAt = "taken_at"
+        case guessedTakenAt = "guessed_taken_at"
+        case md5Sum = "md5sum"
+        case contentType = "content_type"
+        case video
+        case thumbnailURL = "thumbnail_url"
+        case downloadURL = "download_url"
+        case resx, resy
+    }
+}
